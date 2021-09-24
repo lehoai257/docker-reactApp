7c368e7406640e8f1273344279dcebf50b3c381b
System Administrator <root@MacBook-Pro-cua-Le.local>
2021-09-24 20:01:30 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -22,6 +22,7 @@ function App() {
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
+        <p>learn react</p>
       </main>
     </CartProvider>
   );
